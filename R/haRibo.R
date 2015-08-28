d3a7c1c160dcaeaa48472b92c275086fbe497002
Federico Marini <marinif@uni-mainz.de>
2015-08-28 12:18:27 +0200

    haRibo initial commit - version 0.1

---
+++ R/haRibo.R
@@ -0,0 +1,5 @@
+#' @title haRibo
+#' @name haRibo
+#' @docType package
+#' @details List of palettes inspired from color combinations of Haribo Gummy Bears and similar candies
+NULL
---
+++ R/palettes.R
@@ -0,0 +1,62 @@
+#' Complete list of palettes
+#'
+#' Use \code{\link{haribo_palette}} to construct palettes of desired length.
+#'
+#' @export
+haribo_palettes <- list(
+  goldbears = c("#D9030B","#FF2E00","#FEAE01","#FFDF00","#F5EFE3","#A6BC1B"),
+  goldbearsUS = c("#FB0000","#FEC601","#FEE202","#FAF3D7","#08BF0B"),
+  konfekt = c("#79391F","#C1722E","#F8D54B","#E4DEC9","#FB9227","#D96892"),
+  picoballa = c("#DE3135","#70F2FF","#FFBA76","#4B9438","#016565","#FD7D8A","#F5E48A")
+)
+
+#' A palette generator inspired by the colors of Haribo gummy bears
+#'
+#' These are a handful of color palettes from Haribo gummy bear compositions.
+#'
+#' @param n Number of colors desired. If \code{type} is set to "discrete", this
+#'   value will be limited to the number of colors available in the palette.
+#'   If omitted, uses all colours.
+#' @param name Name of desired palette. Choices are:
+#'   \code{goldbears}, \code{goldbearsUS},  \code{konfekt},
+#'   \code{picoballa}
+#' @param type Either "continuous" or "discrete". Use continuous if you want
+#'   to automatically interpolate between colours.
+#' @return A vector of colours.
+#' @export
+#' @keywords colors
+#'
+haribo_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- haribo_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  out <- switch(type,
+                continuous = colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
+}
+
+#' @export
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 3, family = "HersheyScript",font = 3)
+}
